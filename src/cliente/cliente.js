6df485adf9544deea49e77c0130c5680a43fcc55
Kaiper09 <gunnarkaipper@gmail.com>
2025-06-16 18:14:39 -0300

    Add files via upload

--- src/cliente/cliente.js
+++ src/cliente/cliente.js
@@ -1,3 +1,4 @@
+
 const tabelaCliente = document.getElementById('clientesTableDados');
 
 const modalCpfCliente = document.getElementById('cliente-cpf');
@@ -5,6 +6,7 @@ const modalNomeCliente = document.getElementById('cliente-nome');
 const modalNascimentoCliente = document.getElementById('cliente-nascimento');
 const modalNumeroCliente= document.getElementById('cliente-numero')
 const modalCidadeCliente= document.getElementById('cliente-cidade')
+const modalSituacaoCliente= document.getElementById('cliente-situacao')
 
 
 const botaoLimpar = document.getElementById('btn-limpar');
@@ -18,18 +20,28 @@ botaoLimpar.addEventListener('click', limpardados )
 
 
 
-function mostrarDetalhes(cpf, nome, nascimento, numero, cidade) {
+function mostrarDetalhes(cpf, nome, nascimento, numero, cidade, situacao) {
     console.log(nascimento)
     modalCpfCliente.value = cpf;
     modalNomeCliente.value = nome;
-    modalNascimentoCliente.value = nascimento.toISOString().slice(0, 10);
+
+    if (nascimento instanceof Date && !isNaN(nascimento)) {
+        modalNascimentoCliente.value = nascimento.toISOString().slice(0, 10);
+        //Ela verifica se o valor nascimento é uma data válida. Se for, formata a data no 
+        // formato AAAA-MM-DD (padrão HTML). Se não for válida, limpa o campo.
+
+    } else {
+        modalNascimentoCliente.value = '';
+    }
+    //modalNascimentoCliente.value = nascimento.toISOString().slice(0, 10);
     modalNumeroCliente.value= numero;
     modalCidadeCliente.value= cidade;
+    modalSituacaoCliente.value= situacao
     
 }
 
 async function limpardados(){
-    mostrarDetalhes('','','')
+     mostrarDetalhes(' ',' ',' ',' ', ' ', ' ')
 }
 
 async function salvarCliente() {
@@ -42,14 +54,14 @@ async function salvarCliente() {
         }
         await window.mecanicaAPI.inserirCliente(novoNome,novaNascimento)
         carregarClientes();
-        mostrarDetalhes('','','')
+         mostrarDetalhes(' ',' ',' ',' ', ' ', ' ')
         return
     }
     else{
         console.log("alterando")
         await atualizarCliente()
         carregarClientes()
-        mostrarDetalhes('','','')
+         mostrarDetalhes(' ',' ',' ',' ', ' ', ' ')
     }
 }
 
@@ -118,11 +130,15 @@ function criarLinhaCliente(clientes) {
     celulaCidade.textContent= clientes.cidade;
     linha.appendChild(celulaCidade)
 
+    const celulaSituacao= document.createElement("td");
+    celulaSituacao.textContent= clientes.situacao
+    linha.appendChild(celulaSituacao)
+
     //botao de modificar
     const celulaBotao = document.createElement("td");
     const botao = document.createElement("button");
     botao.addEventListener("click",
-        function () { mostrarDetalhes(clientes.cpf, clientes.nome, clientes.nascimento, clientes.numero, clientes.cidade) }
+        function () { mostrarDetalhes(clientes.cpf, clientes.nome, clientes.nascimento, clientes.numero, clientes.cidade, clientes.situacao) }
     );
     botao.textContent = '';
 
